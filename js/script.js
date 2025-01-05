0de86850a67ad1eb8b9598ac7f685ce6dc47040b
MaksimKhandeshyn <145330399+MaksimKhandeshyn@users.noreply.github.com>
2025-01-05 13:20:03 +0200

    make hw

--- js/script.js
+++ js/script.js
@@ -23,23 +23,23 @@ const bankAccount = {
 adding.addEventListener("click", bankAccount.deposit);
 deducation.addEventListener("click", bankAccount.withdraw);
 //--2--
-// const weather = {
-//   temperature: 10,
-//   humidity: 1,
-//   windSpeed: 20,
-// };
-// weather.showTemp = function ourTem() {
-//   let temperature = prompt("Enter your temperature now");
-//   if (temperature > 0) {
-//     alert("Temperature highter than 0");
-//     return false;
-//   } else {
-//     alert("Temperature lower than 0");
-//     return true;
-//   }
-// };
-// weather.showTemp();
-// console.log(weather);
+const weather = {
+  temperature: 10,
+  humidity: 1,
+  windSpeed: 20,
+};
+weather.showTemp = function ourTem() {
+  let temperature = prompt("Enter your temperature now");
+  if (temperature > 0) {
+    alert("Temperature highter than 0");
+    return false;
+  } else {
+    alert("Temperature lower than 0");
+    return true;
+  }
+};
+weather.showTemp();
+console.log(weather);
 //--3--
 const user = {
   mail: prompt("Enter your email"),
@@ -59,3 +59,27 @@ user.check = function rights() {
   }
 };
 user.check();
+
+//--4--
+const movie = {
+  title: "Kanashibara",
+  director: "Angelina Shen",
+  year: 2024,
+  rating: Number(prompt("Enter rating to this book/movie")),
+};
+movie.ratingComment = function ratingFeedback() {
+  if (movie.rating > 8) {
+    alert(
+      `You have rated this movie as ${movie.rating}. The ${movie.title}, Year: ${movie.year}, director ${movie.director}`
+    );
+    return true;
+  } else {
+    alert(
+      `Title: ${this.title}, year: ${this.year}, director: ${this.director}, your rating ${this.rating}`
+    );
+    console.log(movie);
+    console.log("Fuciton return false");
+    return false;
+  }
+};
+movie.ratingComment();
